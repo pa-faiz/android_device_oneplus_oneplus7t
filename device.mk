c1416d1005cc5cdfd8f2d65dd6e3e5b75aecd931
Omkar Chandorkar <gotenksIN@aospa.co>
2022-09-01 22:34:14 +0900

    oneplus7t: overlay: set stock fingerprint
    Change-Id: I2e57c1f66ce7a1e2aff1779a5623d7992bf33098

--- device.mk
+++ device.mk
@@ -10,6 +10,7 @@ $(call inherit-product, vendor/oneplus/oneplus7t/oneplus7t-vendor.mk)
 
 # Overlays
 PRODUCT_PACKAGES += \
+    AOSPAOnePlus7TFrameworks \
     AOSPAOnePlus7TSystemUI \
     OnePlus7TFrameworks \
     OnePlus7TSettings \
